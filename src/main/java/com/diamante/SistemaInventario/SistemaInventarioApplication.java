17fabe6c1d9dd9254637c2bdff57ea2f392f360e
LucasViana130 <lucasgviana1@gmail.com>
2026-03-30 18:36:28 -0300

    Fix commit

--- src/main/java/com/diamante/SistemaInventario/SistemaInventarioApplication.java
+++ src/main/java/com/diamante/SistemaInventario/SistemaInventarioApplication.java
@@ -3,14 +3,16 @@ package com.diamante.SistemaInventario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SistemaInventarioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaInventarioApplication.class, args);
 	}
-	@GetMapping("/")
+	@GetMapping("/healthcheck")
 	public String healthCheck() {
 		return "Server UP";
 	}
